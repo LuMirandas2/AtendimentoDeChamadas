c5e875a8d56b10ed3722f70874a0d2e3f6a389b1
LuMirandas2 <luanna.alvesmiranda0@gmail.com>
2022-11-26 17:23:11 -0300

    Configurando

---
+++ src/services/firebaseConnection.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDoZNpXJA54ab3G6Mbl56QEySGjx05winQ",
+  authDomain: "teste-ea7de.firebaseapp.com",
+  projectId: "teste-ea7de",
+  storageBucket: "teste-ea7de.appspot.com",
+  messagingSenderId: "461398049427",
+  appId: "1:461398049427:web:f8e7899d8f2e10bf400366",
+  measurementId: "G-GJLPK1H4M0",
+};
+
+if (!firebase.app.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;
